231fb07d1c619a64a46f6ebc7374c9bc848f144f
LABANI <labanidas2001@gmail.com>
2024-12-22 10:33:33 +0530

    calling connectDB when server connected

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,21 @@
 import express from "express";
+import dotenv from "dotenv";
+
+import { connectDB } from "./lib/db.js";
 import authRoutes from './routes/auth.route.js';
 
+
+dotenv.config();
+
 const app = express();
+app.use(express.json());
 
 app.use("/app/auth", authRoutes)
 
 
-const PORT = 5001
+const PORT = process.env.PORT
+
 app.listen(PORT, ()=>{
     console.log(`Server is running on port number ${PORT}`);
+    connectDB;
 })
